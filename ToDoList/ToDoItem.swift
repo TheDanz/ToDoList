8d7129e30fa2b33b35aaeb5ec0a360aefff741ff
TheDanz <ryumin_danil@icloud.com>
2024-06-21 16:09:11 +0300

    HW1: Create struct ToDoItem

--- ToDoList/ToDoItem.swift
+++ ToDoList/ToDoItem.swift
@@ -1,1 +1,34 @@
 import Foundation
+
+struct ToDoItem {
+    enum Importance: String {
+        case important = "important"
+        case normal = "normal"
+        case unimportant = "unimportant"
+    }
+    
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let creationDate: Date
+    let modificationDate: Date?
+    
+    init(id: String = UUID().uuidString,
+         text: String,
+         importance: Importance = .normal,
+         deadline: Date? = nil,
+         isDone: Bool = false,
+         modificationDate: Date? = nil
+    ) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.creationDate = Date()
+        self.modificationDate = modificationDate
+    }
+}
+
