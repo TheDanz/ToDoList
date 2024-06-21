952f497cf9dfa4881482491994a326eb402b6de2
TheDanz <ryumin_danil@icloud.com>
2024-06-21 18:55:32 +0300

    HW1: Implement var json & func parse(json: Any)

--- ToDoList/ToDoItem.swift
+++ ToDoList/ToDoItem.swift
@@ -32,3 +32,53 @@ struct ToDoItem {
     }
 }
 
+
+// MARK: ToDoItem - JSON
+extension ToDoItem {
+    var json: Any {
+        
+        var dict: [String: Any] = [
+            "id": id,
+            "text": text,
+            "isDone": isDone
+        ]
+        
+        if importance != .normal {
+            dict["importance"] = importance.rawValue
+        }
+        
+        if let deadline = deadline {
+            dict["deadline"] = deadline.ISO8601Format()
+        }
+        
+        return dict
+    }
+    
+    static func parse(json: Any) -> ToDoItem? {
+        
+        guard let dict = json as? [String: Any],
+              let id = dict["id"] as? String,
+              let text = dict["text"] as? String,
+              let isDone = dict["isDone"] as? Bool
+        else { return nil }
+        
+        var importance: Importance?
+        if let unwrappedImportance = dict["importance"] as? String {
+            importance = Importance(rawValue: unwrappedImportance.lowercased())
+        }
+                
+        var deadline: Date?
+        if let unwrappedDeadline = dict["deadline"] as? String {
+            let formatter = ISO8601DateFormatter()
+            deadline = formatter.date(from: unwrappedDeadline)
+        }
+        
+        let toDoItem = ToDoItem(id: id,
+                                text: text,
+                                importance: importance ?? .normal,
+                                deadline: deadline,
+                                isDone: isDone
+        )
+        return toDoItem
+    }
+}
