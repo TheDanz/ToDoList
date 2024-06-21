86680a94c6f552e49f7054b2e3a0805684996d9a
TheDanz <ryumin_danil@icloud.com>
2024-06-21 15:57:15 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Danil Ryumin on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
