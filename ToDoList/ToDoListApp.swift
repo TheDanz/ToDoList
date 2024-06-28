b13cb6d45837dafd15dea5b1ef2bf04bd1019a7c
TheDanz <ryumin_danil@icloud.com>
2024-06-28 19:44:51 +0300

    HW2: Create MainView & DetailsView

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -1,17 +1,10 @@
-//
-//  ToDoListApp.swift
-//  ToDoList
-//
-//  Created by Danil Ryumin on 21.06.2024.
-//
-
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
