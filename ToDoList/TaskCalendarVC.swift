c2a017a01e0f78e92754f0056be919033c942c7c
TheDanz <ryumin_danil@icloud.com>
2024-07-04 02:26:01 +0300

    HW3: Add dateCollectionView on TaskCalendarVC

--- ToDoList/TaskCalendarVC.swift
+++ ToDoList/TaskCalendarVC.swift
@@ -1,5 +1,4 @@
 import UIKit
-import SwiftUI
 
 class TaskCalendarVC: UIViewController {
     
@@ -13,6 +12,18 @@ class TaskCalendarVC: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    lazy var dateCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 70, height: 70)
+        layout.minimumLineSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
         
     lazy var taskTableView = {
         let tableView = UITableView()
@@ -25,29 +36,75 @@ class TaskCalendarVC: UIViewController {
         
         self.title = "Мои дела"
         
-        taskTableView.delegate = self
-        taskTableView.dataSource = self
-        
-        view.addSubview(taskTableView)
-        
+        setupAllSubviews()
         setupAllConstraints()
     }
     
-    private func setupAllConstraints() {
-        setupTasksTableViewConstraints()
+    private func setupAllSubviews() {
+        setupDateCollectionView()
+        setupTaskTableView()
     }
     
-    private func setupTasksTableViewConstraints() {
-        taskTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+    private func setupAllConstraints() {
+        setupDateCollectionViewConstraints()
+        setupTaskTableViewConstraints()
+    }
+    
+    private func setupDateCollectionView() {
+        dateCollectionView.delegate = self
+        dateCollectionView.dataSource = self
+        dateCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.addSubview(dateCollectionView)
+    }
+    
+    private func setupTaskTableView() {
+        taskTableView.delegate = self
+        taskTableView.dataSource = self
+        view.addSubview(taskTableView)
+    }
+    
+    private func setupDateCollectionViewConstraints() {
+        dateCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        dateCollectionView.bottomAnchor.constraint(equalTo: taskTableView.topAnchor, constant: 0).isActive = true
+        dateCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        dateCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        dateCollectionView.heightAnchor.constraint(equalToConstant: 90).isActive = true
+}
+    
+    private func setupTaskTableViewConstraints() {
+        taskTableView.topAnchor.constraint(equalTo: dateCollectionView.bottomAnchor, constant: 0).isActive = true
         taskTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         taskTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         taskTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
     }
 }
 
-extension TaskCalendarVC: UITableViewDelegate {
+
+// MARK: Collection View
+extension TaskCalendarVC: UICollectionViewDelegate { }
+
+extension TaskCalendarVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        model.groupedTasksByDeadline.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        let sortedDates = model.groupedTasksByDeadline.keys.sorted()
+        let date = sortedDates[indexPath.row]
+        
+        let title = UILabel()
+        title.frame = cell.bounds
+        title.text = date
+        cell.contentView.addSubview(title)
+        
+        return cell
+    }
 }
+
+// MARK: Table View
+extension TaskCalendarVC: UITableViewDelegate { }
 
 extension TaskCalendarVC: UITableViewDataSource {
     
