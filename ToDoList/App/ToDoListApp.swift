08af4e0f42d1be23d7e732dc9d998fecbb8db783
TheDanz <ryumin_danil@icloud.com>
2024-08-01 12:00:33 +0300

    refactor: Create setupLogging func

--- ToDoList/App/ToDoListApp.swift
+++ ToDoList/App/ToDoListApp.swift
@@ -3,17 +3,22 @@ import CocoaLumberjackSwift
 
 @main
 struct ToDoListApp: App {
+    
     init() {
-        DDLog.add(DDOSLogger.sharedInstance)
-        let fileLogger: DDFileLogger = DDFileLogger()
-        fileLogger.rollingFrequency = TimeInterval(60 * 60 * 24 * 7)
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
-        DDLog.add(fileLogger)
+        setupLogging()
     }
     
     var body: some Scene {
         WindowGroup {
             MainView()
         }
+    }
+    
+    private func setupLogging() {
+        DDLog.add(DDOSLogger.sharedInstance)
+        let fileLogger: DDFileLogger = DDFileLogger()
+        fileLogger.rollingFrequency = TimeInterval(60 * 60 * 24 * 7)
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.add(fileLogger)
     }
 }
